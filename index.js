e83d4d774240f593e0eda86cf2f9710a699c7058
Zastinian <zastinian@gmail.com>
2023-04-13 21:58:41 -0600

    feat: Help and Index

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
-import {Embed} from "guilded.js";
 import Hedystia from "./src/lib/Hedystia.js";
-
-const client = new Hedystia();
-
-client.on("messageCreated", (message) => {
-	const author = message.author;
-	if (!author) return;
-	console.log(author.type);
-	if (message.content === "test") {
-		const embed = new Embed({
-			title: "Test",
-			description: "Testing!",
-		});
-		return message.reply({
-			embeds: [embed],
-		});
-	}
-});
-
-client.login();
+new Hedystia();
---
+++ src/commands/data/help.js
@@ -0,0 +1,32 @@
+import {Embed} from "guilded.js";
+
+export default {
+	name: "help",
+	description: "Help",
+	run: async (Hedystia, message, args) => {
+		const embed = new Embed()
+			.setColor(0x0099ff)
+			.setTitle("Help Menu")
+			.setAuthor(
+				"Name",
+				"https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTMyYzU4YTVjNjNlZWUwZTgwN2ZiMDgxYzVlOGE0NGRhYTM3MmE1NCZjdD1z/K9svE9i7P3Ox2/giphy.gif",
+				"https://docs.mresmile.com/docs/client/start"
+			)
+			.setURL("https://docs.mresmile.com/docs/client/start")
+			.setDescription("Help Description")
+			.setThumbnail(
+				"https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTMyYzU4YTVjNjNlZWUwZTgwN2ZiMDgxYzVlOGE0NGRhYTM3MmE1NCZjdD1z/K9svE9i7P3Ox2/giphy.gif"
+			)
+			.addField("Command", "/help", true)
+			.addField("Command", "/user", true)
+			.setImage("https://c.tenor.com/yi5btxWVAwwAAAAC/tenor.gif")
+			.setTimestamp()
+			.setFooter(
+				"Footer",
+				"https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZTMyYzU4YTVjNjNlZWUwZTgwN2ZiMDgxYzVlOGE0NGRhYTM3MmE1NCZjdD1z/K9svE9i7P3Ox2/giphy.gif"
+			);
+		return message.reply({
+			embeds: [embed],
+		});
+	},
+};
