7fca73bb4d041815c19e0e8c3fd26bbfcb424a73
Zastinian <zastinian@gmail.com>
2023-04-13 21:46:31 -0600

    feat: Command and Config

--- src/events/app/commands.js
+++ src/events/app/commands.js
@@ -1,9 +1,23 @@
+import config from "../../config/index.js";
+
 const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
 export default {
 	event_name: "messageCreated",
 	run: (Hedystia, message) => {
 		try {
+			const author = message.author;
+			if (!author) return;
+			if (author.type === 0) return;
+			const prefixRegex = new RegExp(`^(<@!?${Hedystia.user.id}>|${escapeRegex(config.PREFIX)})\\s*`);
+			if (!prefixRegex.test(message.content)) return;
+			const [, matchedPrefix] = message.content.match(prefixRegex);
+			const p = matchedPrefix.length;
+			const args = message.content.slice(p).trim().split(/ +/);
+			const commandName = args.shift().toLowerCase();
+			const command = Hedystia.commands.get(commandName) || Hedystia.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName));
+			if (!command) return;
+			command.run(Hedystia, message, args, p);
 		} catch (err) {
 			return;
 		}
